5c23856d352dfc9d774bde8d482a88990ab9306e
Randhir Singh <randhirkumars@yahoo.com>
2021-09-05 22:35:42 +0530

    Update Microservice1Steps.java

--- src/test/java/microservice1/steps/Microservice1Steps.java
+++ src/test/java/microservice1/steps/Microservice1Steps.java
@@ -1,7 +1,6 @@
 package microservice1.steps;
 
 import static util.Util.jsonTemplate;
-import static util.Util.stringFromFile;
 
 import java.io.IOException;
 import java.util.HashMap;
@@ -24,13 +23,13 @@ import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 import util.RequestSpecificationFactory;
 
-public class IDVerifySteps {
+public class Microservice1Steps {
 
     private final World world;
     private final Properties envConfig;
     private RequestSpecification request;
 
-    public IDVerifySteps(World world) {
+    public Microservice1Steps(World world) {
         this.world = world;
         this.envConfig = World.envConfig;
         this.world.featureContext = World.threadLocal.get();
@@ -43,10 +42,10 @@ public class IDVerifySteps {
 
     @Given("caller presents a valid OAuth2 token")
     public void getOAuth2Token() {
-        String grantType = envConfig.getProperty("idverify-grant_type");
-        String clientId = envConfig.getProperty("idverify-client_id");
-        String clientSecret = envConfig.getProperty("idverify-client_secret");
-        String accessTokenUrl = envConfig.getProperty("idverify-access_token_url");
+        String grantType = envConfig.getProperty("microservice1-grant_type");
+        String clientId = envConfig.getProperty("microservice1-client_id");
+        String clientSecret = envConfig.getProperty("microservice1-client_secret");
+        String accessTokenUrl = envConfig.getProperty("microservice1-access_token_url");
 
         String body = String.format("grant_type=%s&client_secret=%s&client_id=%s", grantType, clientSecret, clientId);
 
@@ -63,18 +62,6 @@ public class IDVerifySteps {
         world.scenarioContext.put("accessToken", accessToken);
     }
 
-    @Given("mock is enabled on the IdVerify Service")
-    public void enableMock() {
-        world.scenarioContext.put("mock", "true");
-        world.scenarioContext.put("dblk", "true");
-    }
-
-    @Given("mock is disabled on the IdVerify Service")
-    public void disableMock() {
-        world.scenarioContext.put("mock", "false");
-        world.scenarioContext.put("dblk", "true");
-    }
-
     @Given("product id is {string}")
     public void productId(String productId) {
         world.scenarioContext.put("productId", productId);
@@ -83,75 +70,20 @@ public class IDVerifySteps {
     @Given("a person with demographic details")
     public void getDemogLiteData(@Transpose DataTable dataTable) throws IOException {
         List<Map<String, String>> data = dataTable.asMaps(String.class, String.class);
-        String nik = data.get(0).get("nik");
+        String nik = data.get(0).get("adhar_id");
         String fullName = data.get(0).get("full_name");
         String dob = data.get(0).get("dob");
         String phoneNo = data.get(0).get("phone_no");
         String email = data.get(0).get("email");
 
         Map<String, Object> valuesToTemplate = new HashMap<>();
-        valuesToTemplate.put("nik", nik);
+        valuesToTemplate.put("adhar_id", nik);
         valuesToTemplate.put("full_name", fullName);
         valuesToTemplate.put("dob", dob);
         valuesToTemplate.put("phone_no", phoneNo);
         valuesToTemplate.put("email", email);
 
-        String jsonAsString = jsonTemplate(envConfig.getProperty("idverify-demog_lite_request"), valuesToTemplate);
-
-        world.scenarioContext.put("requestStr", jsonAsString);
-    }
-
-    @Given("a person with full demographic details")
-    public void getDemogData(@Transpose DataTable dataTable) throws IOException {
-        List<Map<String, String>> data = dataTable.asMaps(String.class, String.class);
-        String nik = data.get(0).get("nik");
-        String fullName = data.get(0).get("full_name");
-        String dob = data.get(0).get("dob");
-        String phoneNo = data.get(0).get("phone_no");
-        String email = data.get(0).get("email");
-        String motherMaidenName = data.get(0).get("mother_maiden_name");
-        String familyCardNo = data.get(0).get("family_card_no");
-        String address = data.get(0).get("address");
-        String village = data.get(0).get("village");
-        String district = data.get(0).get("district");
-        String city = data.get(0).get("city");
-        String province = data.get(0).get("province");
-
-        Map<String, Object> valuesToTemplate = new HashMap<>();
-        valuesToTemplate.put("nik", nik);
-        valuesToTemplate.put("full_name", fullName);
-        valuesToTemplate.put("dob", dob);
-        valuesToTemplate.put("phone_no", phoneNo);
-        valuesToTemplate.put("email", email);
-        valuesToTemplate.put("mother_maiden_name", motherMaidenName);
-        valuesToTemplate.put("family_card_no", familyCardNo);
-        valuesToTemplate.put("address", address);
-        valuesToTemplate.put("village", village);
-        valuesToTemplate.put("district", district);
-        valuesToTemplate.put("city", city);
-        valuesToTemplate.put("province", province);
-
-        String jsonAsString = jsonTemplate(envConfig.getProperty("idverify-demog_request"), valuesToTemplate);
-
-        world.scenarioContext.put("requestStr", jsonAsString);
-    }
-
-    @Given("a person with valid face image")
-    public void getFaceMatchData(@Transpose DataTable dataTable) throws IOException {
-        List<Map<String, String>> data = dataTable.asMaps(String.class, String.class);
-        String nik = data.get(0).get("nik");
-        String phoneNo = data.get(0).get("phone_no");
-        String email = data.get(0).get("email");
-        String faceImageBase64 = stringFromFile(System.getProperty("user.dir")
-                + envConfig.getProperty("idverify-face_image_base64"));
-
-        Map<String, Object> valuesToTemplate = new HashMap<>();
-        valuesToTemplate.put("nik", nik);
-        valuesToTemplate.put("phone_no", phoneNo);
-        valuesToTemplate.put("email", email);
-        valuesToTemplate.put("face_image", faceImageBase64);
-
-        String jsonAsString = jsonTemplate(envConfig.getProperty("idverify-face_match_request"), valuesToTemplate);
+        String jsonAsString = jsonTemplate(envConfig.getProperty("microservice1-demog_request"), valuesToTemplate);
 
         world.scenarioContext.put("requestStr", jsonAsString);
     }
@@ -165,7 +97,7 @@ public class IDVerifySteps {
         world.scenarioContext.put("requestStr", payload.toString());
     }
 
-    @When("request is submitted for demog verification")
+    @When("request is submitted for demographic verification")
     public void submitForDemogLite() {
         String accessToken = world.scenarioContext.get("accessToken").toString();
         String payload = world.scenarioContext.get("requestStr").toString();
@@ -181,74 +113,8 @@ public class IDVerifySteps {
                 .queryParam("dblk", dblk)
                 .body(payload)
                 .contentType(ContentType.JSON)
-                .when().post(envConfig.getProperty("idverify-service_url")
-                        + envConfig.getProperty("idverify-demog_lite_api"));
-
-        world.scenarioContext.put("response", response);
-    }
-
-    @When("request is submitted for full demog verification")
-    public void submitForFullDemog() {
-        String accessToken = world.scenarioContext.get("accessToken").toString();
-        String payload = world.scenarioContext.get("requestStr").toString();
-        String mock = world.scenarioContext.get("mock").toString();
-        String dblk = world.scenarioContext.get("dblk").toString();
-        String productId = world.scenarioContext.get("productId").toString();
-
-        Response response = request
-                .accept(ContentType.JSON)
-                .header("Authorization", "Bearer " + accessToken)
-                .header("X-Product-Id", productId)
-                .queryParam("mock", mock)
-                .queryParam("dblk", dblk)
-                .body(payload)
-                .contentType(ContentType.JSON)
-                .when().post(envConfig.getProperty("idverify-service_url")
-                        + envConfig.getProperty("idverify-demog_api"));
-
-        world.scenarioContext.put("response", response);
-    }
-
-    @When("request is submitted for face match to Dukcapil")
-    public void submitForFaceMatchDukcapil() {
-        String accessToken = world.scenarioContext.get("accessToken").toString();
-        String payload = world.scenarioContext.get("requestStr").toString();
-        String mock = world.scenarioContext.get("mock").toString();
-        String dblk = world.scenarioContext.get("dblk").toString();
-        String productId = world.scenarioContext.get("productId").toString();
-
-        Response response = request
-                .accept(ContentType.JSON)
-                .header("Authorization", "Bearer " + accessToken)
-                .header("X-Product-Id", productId)
-                .queryParam("mock", mock)
-                .queryParam("dblk", dblk)
-                .body(payload)
-                .contentType(ContentType.JSON)
-                .when().post(envConfig.getProperty("idverify-service_url")
-                        + envConfig.getProperty("idverify-face_match_api"));
-
-        world.scenarioContext.put("response", response);
-    }
-
-    @When("request is submitted for face match to Portal")
-    public void submitForFaceMatchPortal() {
-        String accessToken = world.scenarioContext.get("accessToken").toString();
-        String payload = world.scenarioContext.get("requestStr").toString();
-        String mock = world.scenarioContext.get("mock").toString();
-        String dblk = world.scenarioContext.get("dblk").toString();
-        String productId = world.scenarioContext.get("productId").toString();
-
-        Response response = request
-                .accept(ContentType.JSON)
-                .header("Authorization", "Bearer " + accessToken)
-                .header("X-Product-Id", productId)
-                .queryParam("mock", mock)
-                .queryParam("dblk", dblk)
-                .body(payload)
-                .contentType(ContentType.JSON)
-                .when().post(envConfig.getProperty("idverify-service_url")
-                        + envConfig.getProperty("idverify-face_match_local_api"));
+                .when().post(envConfig.getProperty("microservice1-service_url")
+                        + envConfig.getProperty("microservice1-demog_api"));
 
         world.scenarioContext.put("response", response);
     }
@@ -276,53 +142,5 @@ public class IDVerifySteps {
         String locationHeader = response.getHeader("Location");
         Assert.assertNotNull(locationHeader);
         world.scenarioContext.put("location", locationHeader);
-    }
-
-    @When("status is checked")
-    public void checkStatus() throws InterruptedException {
-        String accessToken = world.scenarioContext.get("accessToken").toString();
-        String productId = world.scenarioContext.get("productId").toString();
-        String statusUrl = world.scenarioContext.get("location").toString();
-
-        Response response = null;
-        do {
-            request = RequestSpecificationFactory.getInstance(world);
-            response = request
-                    .accept(ContentType.JSON)
-                    .header("Authorization", "Bearer " + accessToken)
-                    .header("X-Product-Id", productId)
-                    .when().get(envConfig.getProperty("idverify-service_url")
-                            + statusUrl);
-
-            if (response.then().extract().statusCode() == 200) {
-                String responseString = response.then().extract().asString();
-                String message = new JSONObject(responseString).getJSONObject("data").getString("message");
-                if ("IN_PROGRESS".equals(message)) {
-                    Thread.sleep(1000);
-                } else {
-                    break;
-                }
-            } else {
-                break;
-            }
-        } while (true);
-
-        world.scenarioContext.put("status_response", response);
-    }
-
-    @Then("face match status is {string}")
-    public void faceMatchResult(String status) {
-        Response response = (Response) world.scenarioContext.get("status_response");
-        String responseString = response.then().extract().asString();
-        String actualStatus = new JSONObject(responseString).getJSONObject("data").getString("status");
-        Assert.assertEquals(actualStatus, status);
-    }
-
-    @Then("face match result is returned")
-    public void faceMatchResult() {
-        Response response = (Response) world.scenarioContext.get("status_response");
-        String responseString = response.then().extract().asString();
-        JSONObject result = new JSONObject(responseString).getJSONObject("data").getJSONObject("result");
-        Assert.assertNotNull(result);
     }
 }
